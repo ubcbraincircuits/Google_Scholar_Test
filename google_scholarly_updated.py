00502e64ce3305938747087a52c410937852e409
forbesashli <44144720+forbesashli@users.noreply.github.com>
2021-08-09 16:16:02 -0700

    updated

--- google_scholarly_updated.py
+++ google_scholarly_updated.py
@@ -2,9 +2,42 @@
 Script to get h-index, i-index, and recent publications for desired researchers.
 This is the updated code that will use the most recent version of scholarly. That is version 1.2.0
 """
-!pip install scholarly
+#!pip install scholarly
+import csv
+
+def append_method(fname):
+  new_name = ""
+  list_of_file = list(fname)
+  for item in list_of_file:
+      if (item == '.'):
+          break
+      new_name = new_name + item
+
+  new_name = new_name +'_output.csv'
+  return new_name
+
+def pull_names(fname):
+  j = 0
+  with open(fname, encoding ="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter =',')
+    for row in csv_reader:
+     j = j +1
+
+  author_names= ['0']*j
+  i = 0
+  with open(fname, encoding ="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter =',')
+    for row in csv_reader:
+        if (len(row) == 1):
+            author_names[i]=row[0]
+            i = i +1
+
+  return author_names
+
+
 
 def scholar_search(fname,author_names,m_name):
+
   if (m_name == "indicies"):
     index_method(fname,author_names)
     return
@@ -27,7 +60,7 @@ def index_method(fname,author_names):
   from scholarly import scholarly
   import csv
 
-  query_row = ['Name', 'H Index', 'H Index 5 years', 'I Index', 'I Index 5 years', 'Pub 1','Pub 2','Pub 3']
+  query_row = ['Name', 'H Index', 'H Index 5 years', 'I Index', 'I Index 5 years']
 
   # This creates/opens the file with filename with the intention to write to the csv_file
   # The encoding allows the characters to be properly written to the csv_file
@@ -40,7 +73,16 @@ def index_method(fname,author_names):
 
       # Perferms the search for the indices of the author, hindex....
       search_query = scholarly.search_author(author_string)
-      author = next(search_query)
+      #search_query = scholarly.search_author(author_string[0])
+
+      try :
+        author = next(search_query)
+      except (RuntimeError,TypeError,StopIteration):
+        row = [author_string,'no information found']# + recent_pubs
+        csv_writer.writerow(row)
+      
+        
+      
       objref = scholarly.fill(author, sections=['indices'])
 
       hindex = objref.get("hindex")
@@ -48,31 +90,8 @@ def index_method(fname,author_names):
       i10index = author.get("i10index")
       i10index5 = author.get("i10index5y")
 
-      pubs = scholarly.fill(author, sections=['publications'])
-      
-      # Intialize the dictonary
-      dict1 = {}
-
-      # For the amount of publications per author, write the year and title to the dictionary 
-      for pub in author['publications']:
-        # Only include publications that have a documented year of publishing
-        if 'pub_year' in pub['bib']:
-          year_pub = pub['bib']['pub_year']
-          title_pub = pub['bib']['title']
-
-          # Write these to the dictionary using the title as the key to avoid repeats of keys
-          dict1[title_pub] = year_pub
-
-
-      # Sort the dictionary by year in order to have the most recent publications at the top
-      # Note since this is only done by year, it can not get down to say month or date of publication
-      sort_dict = sorted(dict1.items(), key=lambda x: x[1], reverse=True)
-      
-      # Only take the three most recent
-      recent_pubs = sort_dict[0:3]
-
       # Write them to the csv file
-      row = [author_string,hindex,hindex5,i10index,i10index5] + recent_pubs
+      row = [author_string,hindex,hindex5,i10index,i10index5]
       csv_writer.writerow(row)
 
 # index_method tales uses the parameter and outputs the three most recent publications by the authors sepcified
--- scholar_client.py
+++ scholar_client.py
@@ -5,11 +5,18 @@ This scrip is the 'client' and will be the code that we will aim to have researc
 """
 
 #Edit the file name to specify where the retrieved information will save too.
-fname = 'google_scholar_data_test.csv'
+fname = 'google_scholar_data_names.csv'
 
 #Edit this array with names of the researchers in which you are looking to retreve information on
-author_names = ['Brian MacVicar', 'Jeffrey LeDue', 'Tim H. Muprhy', 'Tim Murphry']
+#author_names = ['Brian MacVicar', 'Jeffrey LeDue', 'Tim H. Muprhy', 'Tim Murphry']
 
 from google_scholarly_updated import scholar_search
+from google_scholarly_updated import pull_names
+from google_scholarly_updated import append_method
+from scholarly import scholarly
+
+author_names = pull_names(fname)
+#scholar_search(fname,'indicies')
+fname = append_method(fname)
 
 scholar_search(fname,author_names,'indicies')
